6865468da227778292f04766fc0a613d51bea059
darrentran <dtran1@ualberta.ca>
2019-11-01 14:27:04 -0600

    changed compare function to struct

---
+++ distwc-test-cases-lab/threadpool.cpp
@@ -0,0 +1,124 @@
+#include "threadpool.h"
+#include <pthread.h>
+
+ThreadPool_t *ThreadPool_create(int num){
+
+    // Create work queue object
+    ThreadPool_work_queue_t workQueue;
+    workQueue.queue = std::queue<ThreadPool_work_t>();
+
+    //  Create threadpool object
+    ThreadPool_t *tp = new ThreadPool_t;
+
+    tp->work_queue = workQueue;
+    tp->stop_running = false;
+    tp->pool = std::vector<pthread_t>(num);
+    tp->live_threads = num;
+    tp->work_mutex = PTHREAD_MUTEX_INITIALIZER;
+    tp->work_available_cond = PTHREAD_COND_INITIALIZER;
+
+    //  Create mapper threads
+    for(int i = 0; i < num; i++) {
+        pthread_create(&tp->pool.at(i), NULL, (void *(*)(void *))Thread_run, tp);
+    }
+
+
+
+    return tp;
+}
+
+void ThreadPool_destroy(ThreadPool_t *tp){
+
+    //  Wait for all live threads to finish
+    while(tp->live_threads != 0);
+
+    // Lock the mutex
+    pthread_mutex_lock(&tp->work_mutex);
+    tp->stop_running = true;
+
+    // Unlock mutex
+    pthread_mutex_unlock(&tp->work_mutex);
+
+    // Destroy mutex and conditions before destroying threadpool object
+    pthread_cond_destroy(&tp->work_available_cond);
+    pthread_mutex_destroy(&tp->work_mutex);
+
+    delete(tp);
+}
+
+bool ThreadPool_add_work(ThreadPool_t *tp, thread_func_t func, void *arg) {
+
+    // Create the work item
+//    ThreadPool_work_t *work = new ThreadPool_work_t(func, arg);
+    ThreadPool_work_t work;
+    work.func = func;
+    work.arg = arg;
+    // lock mutex
+    pthread_mutex_lock(&(tp->work_mutex));
+    // add item to queue
+    tp->work_queue.queue.push(work);
+
+    // let waiting threads know a new item has been added to the queue
+    pthread_cond_signal(&(tp->work_available_cond));
+
+    // unlock mutex
+    pthread_mutex_unlock(&(tp->work_mutex));
+    return true;
+}
+
+ThreadPool_work_t *ThreadPool_get_work(ThreadPool_t *tp) {
+
+    // get the item at front of queue and decrease number of items on queue by 1
+    ThreadPool_work_t *nextWorkItem = &(tp->work_queue.queue.front());
+    tp->work_queue.queue.pop();
+
+    return nextWorkItem->func == NULL && nextWorkItem->arg == NULL ? NULL : nextWorkItem;
+}
+
+void *Thread_run(ThreadPool_t *tp) {
+
+    ThreadPool_work_t *work = NULL;
+
+    // thread will continually run this function until stop is signaled
+    while(true) {
+
+        // lock mutex to access work queue exclusively
+        pthread_mutex_lock(&(tp->work_mutex));
+
+        // break loop is stop condition has been notified
+        if (tp->stop_running) {
+            break;
+        }
+
+        // If there is no work in the queue, wait until there is work available
+        while (tp->work_queue.queue.size() == 0 && !tp->stop_running) {
+            pthread_cond_wait(&(tp->work_available_cond), &(tp->work_mutex));
+        }
+
+        // Get the next work item
+        if(!tp->stop_running) {
+            work = ThreadPool_get_work(tp);
+        }
+
+        //  If the work item is NULL, set termination flag to true
+        //  and wake up all sleeping threads.
+        if(work == NULL) {
+            tp->stop_running = true;
+            pthread_cond_broadcast(&(tp->work_available_cond));
+            break;
+        }
+
+        //  Unlock mutex
+        pthread_mutex_unlock(&(tp->work_mutex));
+
+        // Do the work
+        work->func(work->arg);
+//        delete(work);
+    }
+
+    // Decrease number of live threads, unlock mutex and terminate thread.
+    tp->live_threads--;
+    pthread_mutex_unlock(&(tp->work_mutex));
+    pthread_exit(0);
+}
+
---
+++ distwc-test-cases-lab/threadpool.h
@@ -0,0 +1,90 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+#include <pthread.h>
+#include <stdbool.h>
+#include <vector>
+#include <queue>
+#include "mapreduce.h"
+
+typedef void (*thread_func_t)(void *arg);
+
+typedef struct ThreadPool_work_t {
+//    ThreadPool_work_t(thread_func_t f, void *a){
+//        func = f;
+//        arg = a;
+//    };
+    thread_func_t func;              // The function pointer
+    void *arg;                       // The arguments for the function
+    // TODO: Add other members here if needed
+} ThreadPool_work_t;
+
+typedef struct {
+    // TODO: Add members here
+    std::queue<ThreadPool_work_t> queue;
+//    ThreadPool_work_t front;
+} ThreadPool_work_queue_t;
+
+typedef struct {
+    /**
+     * pool: pool of threads
+     * work_queue: queue of work items
+     * work_mutex: global mutex for threads in pool
+     * available_work_cond: condition for threads to be notified if there are still work items in the queue
+     * threads_working_cond: condition to check if there are still threads performing work
+     * stop_running: boolean to check if thread should stop running
+     **/
+
+    std::vector<pthread_t> pool;
+    ThreadPool_work_queue_t work_queue;
+    pthread_mutex_t  work_mutex;
+    pthread_cond_t   work_available_cond;
+    bool stop_running;
+    int live_threads;
+
+} ThreadPool_t;
+
+
+/**
+* A C style constructor for creating a new ThreadPool object
+* Parameters:
+*     num - The number of threads to create
+* Return:
+*     ThreadPool_t* - The pointer to the newly created ThreadPool object
+*/
+ThreadPool_t *ThreadPool_create(int num);
+
+/**
+* A C style destructor to destroy a ThreadPool object
+* Parameters:
+*     tp - The pointer to the ThreadPool object to be destroyed
+*/
+void ThreadPool_destroy(ThreadPool_t *tp);
+
+/**
+* Add a task to the ThreadPool's task queue
+* Parameters:
+*     tp   - The ThreadPool object to add the task to
+*     func - The function pointer that will be called in the thread
+*     arg  - The arguments for the function
+* Return:
+*     true  - If successful
+*     false - Otherwise
+*/
+bool ThreadPool_add_work(ThreadPool_t *tp, thread_func_t func, void *arg);
+
+/**
+* Get a task from the given ThreadPool object
+* Parameters:
+*     tp - The ThreadPool object being passed
+* Return:
+*     ThreadPool_work_t* - The next task to run
+*/
+ThreadPool_work_t *ThreadPool_get_work(ThreadPool_t *tp);
+
+/**
+* Run the next task from the task queue
+* Parameters:
+*     tp - The ThreadPool Object this thread belongs to
+*/
+void *Thread_run(ThreadPool_t *tp);
+#endif
